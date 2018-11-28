c6ed8be1e5674f3abc1164ef815545bbb149fe28
JR <jrkrelix_01@hotmail.com>
2018-11-27 20:22:31 -0700

    Updates

---
+++ precache-manifest.61d26ca69a206a110aca1877a0e10f1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b4b037386b06734053f",
+    "url": "/rude-map/static/js/runtime~main.1b4b0373.js"
+  },
+  {
+    "revision": "c491047c38644db94176",
+    "url": "/rude-map/static/js/main.c491047c.chunk.js"
+  },
+  {
+    "revision": "fc33d87b282cbc309a83",
+    "url": "/rude-map/static/js/1.fc33d87b.chunk.js"
+  },
+  {
+    "revision": "c491047c38644db94176",
+    "url": "/rude-map/static/css/main.961648b7.chunk.css"
+  },
+  {
+    "revision": "da64aac64ca7a2192da77a66792e46b0",
+    "url": "/rude-map/index.html"
+  }
+];
